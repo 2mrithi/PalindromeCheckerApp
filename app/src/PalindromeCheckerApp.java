f542efb374df61d7f8c219258ad8c6a12d32074f
2mrithi <smrithij2017@gmai.com>
2026-03-09 09:45:35 +0530

    Queue + Stack Based Palindrome Check

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,32 +1,29 @@
-/*
- * Application entry point
- * This is the first method execute by the JVM when the program starts
- * @author Developer
- * @Version 1.0
- */
+import java.util.*;
 
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        public static void main(String[] args) {
 
-            String input = "madam";   // Hardcoded string
-            boolean isPalindrome = true;
+        String input = "civic";
 
-            // Loop only till half of the string length
-            for (int i = 0; i < input.length() / 2; i++) {
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
 
-                // Compare characters from both ends
-                if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-
-            // Print input text
-            System.out.println("Input text: " + input);
-
-            // Print result
-            System.out.println("Is it a Palindrome?: " + isPalindrome);
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
         }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
